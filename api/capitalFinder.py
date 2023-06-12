b46cf56653ce136b6da0e9bdb9bb59d4d80fbfb3
put your github username here <YOUR EMAIL ADDRESS>
2023-06-12 19:58:28 +0000

    done

--- api/capitalFinder.py
+++ api/capitalFinder.py
@@ -1,35 +1,48 @@
+import http.client
+import json
 from http.server import BaseHTTPRequestHandler
-from urllib import parse
-import requests 
+from urllib.parse import urlparse, parse_qs
+
 
 class handler(BaseHTTPRequestHandler):
- 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    countries=[]
-    message="testing"
-    url_path = self.path
-    url_components = parse.urlsplit(url_path)
-    query_list = parse.parse_qsl(url_components.query)
-    my_dict = dict(query_list)
 
-    print(111,my_dict)
-    if 'country' in my_dict:
-      country = my_dict.get('country')
-      url= 'https://restcountries.com/v3.1/name/'
-      res = requests.get(url+country)
-      data = res.json()
-    #   print(222,data)
-    for country in data :
-      capital = country.get['capital']
-      message = str(capital)
-      countries.append(message)
-    print(2222,countries)
+    def do_GET(self):
+        parsed_url = urlparse(self.path)
+        query_params = parse_qs(parsed_url.query)
+
+        if 'country' in query_params:
+            country = query_params['country'][0]
+            capital = get_capital_by_country(country)
+            response = f"The capital of {country} is {capital}."
+        elif 'capital' in query_params:
+            capital = query_params['capital'][0]
+            country = get_country_by_capital(capital)
+            response = f"{capital} is the capital of {country}."
+        else:
+            response = "Invalid request."
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(response.encode('utf-8'))
+        return
 
 
+def get_capital_by_country(country):
+    conn = http.client.HTTPSConnection("restcountries.com")
+    conn.request("GET", "/v3.1/name/" + country)
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    country_data = json.loads(data)[0]
+    capital = country_data["capital"][0]
+    return capital
 
 
-    self.wfile.write(message)
-    return
+def get_country_by_capital(capital):
+    conn = http.client.HTTPSConnection("restcountries.com")
+    conn.request("GET", "/v3.1/capital/" + capital)
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    countries_data = json.loads(data)
+    country = countries_data[0]["name"]["official"]
+    return country
